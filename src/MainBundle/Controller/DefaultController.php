0bf249576ad7c5de139416877ce03bacab2ade1f
Halim <h.izzouzi@live.fr>
2018-06-30 23:46:04 +0200

    nouveau bundle

---
+++ src/MainBundle/Controller/DefaultController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace MainBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('@Main/Default/index.html.twig');
+    }
+}
